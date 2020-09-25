1170c87b4428b2a3be81cf433529f0945d0f62e1
Zenén Hernández Martínez <zenenhernadez85@gmail.com>
2020-09-25 15:30:34 -0400

    iniciando git y comenzando con dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TypeSelectComponent } from './components/type-select/type-select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TypeSelectComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/type-select/type-select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-type-select',
+  templateUrl: './type-select.component.html',
+  styleUrls: ['./type-select.component.scss']
+})
+export class TypeSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
