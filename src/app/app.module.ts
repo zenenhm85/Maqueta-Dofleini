ebf9f3c44538300d9c03fa4c8f578cb5338242a2
Zenén Hernández Martínez <zenenhernadez85@gmail.com>
2020-09-25 19:00:17 -0400

    dashboard terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { TypeSelectComponent } from './components/type-select/type-select.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import { MatInputModule } from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatIconModule} from '@angular/material/icon'; 
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +21,13 @@ import { TypeSelectComponent } from './components/type-select/type-select.compon
     TypeSelectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
